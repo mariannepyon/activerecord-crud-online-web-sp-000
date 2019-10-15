467894e38bd9161a3c22608a4ca7ec70fe348798
Marianne Pyon <mariannepyon@gmail.com>
2019-10-15 20:27:39 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,13 +23,14 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block (args = __)
+def can_be_created_in_a_block (args = (title = "Home Alone", release_date = 1990))
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = "Woo!"
+    m.title = "The Room"
+    m.release_date = 2003
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_10_15_170122) do
 
-  create_table "create_movies", force: :cascade do |t|
+  create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "release_date"
     t.string "director"
